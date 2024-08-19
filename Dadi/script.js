b71afc1c4c463428af7d74dc81eaed1b1729253e
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-19 16:34:01 +0200

    added generator Numbers

--- Dadi/script.js
+++ Dadi/script.js
@@ -1,2 +1,11 @@
 console.log ('JS OK')
 
+// Genero un numero Randon da 1 a 6 per l'utente
+
+const numberUser = Math.floor(Math.random() * 6) + 1;
+console.log(numberUser)
+ 
+// Genero un numero Random da 1 a 6 per il computer
+
+const numberPc = Math.floor(Math.random() * 6) + 1;
+console.log(numberPc)
