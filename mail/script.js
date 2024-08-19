3a3b2b176c0a0e27e294edfef28cf7eb5d587833
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-19 17:14:10 +0200

    Added Array authorized emails

---
+++ mail/script.js
@@ -0,0 +1,7 @@
+console.log ('JS OK')
+
+// Creiamo una lista di mail autorizzate
+
+const emails = ['angelocrisci@js.it' , 'angelocrisci@gmail.com' , 'angelocrisci@libero.it' , 'angelocrisci@hotmail.it']
+
+// 
