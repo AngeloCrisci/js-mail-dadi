a75a35acad9bde8e39c6b58c981efd1dbe0c1c65
Angelo.Crisci <angelocrisci1998@gmail.com>
2024-08-19 17:34:45 +0200

    Added event listener

--- mail/script.js
+++ mail/script.js
@@ -4,4 +4,15 @@ console.log ('JS OK')
 
 const emails = ['angelocrisci@js.it' , 'angelocrisci@gmail.com' , 'angelocrisci@libero.it' , 'angelocrisci@hotmail.it']
 
-// 
+// Elementi del Form
+
+const userEmail = document.getElementById('useremail')
+const confirmButton = document.getElementById('confirm-button')
+
+// Recupero Email tramite il click del buttuon
+
+confirmButton.addEventListener('click' , function(){
+    const emailValue = userEmail.value.trim();
+})
+
+
